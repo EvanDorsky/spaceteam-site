94a8296af608bc83df936185142301790e93b0c8
Evan Dorsky <evan.dorsky@icloud.com>
2016-04-24 22:11:15 -0400

    Working parallax effect, media

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $window = $(window);
+
+    $('[data-type="background"]').each(function(){
+        var $scroll = $(this);
+
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $scroll.data('speed'));
+            var coords = '50% '+ yPos + 'px';
+
+            $scroll.css({ backgroundPosition: coords });
+        });
+    });
+});
